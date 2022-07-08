78f8ca100f5f97d23e555cb75fe4a0a25b6fc5ec
vanfleet-dev <vanfleet.dev@protonmail.com>
2022-07-07 22:01:13 -0700

    gas planet: sprites, orbit

---
+++ project/World/DockingPoint/gas_1.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+export var rotation_speed = PI
+export var rotation_speed2 = PI
+export var rotation_speed3 = PI
+export var rotation_speed4 = PI
+
+func _process(delta):
+	$gas_1_sprite/gas_moon_1.rotation += rotation_speed * delta
+	$gas_1_sprite/gas_moon_2.rotation += rotation_speed2 * delta
+	$gas_1_sprite/gas_moon_3.rotation += rotation_speed3 * delta
+	$gas_1_sprite/gas_1_rings.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings2.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings3.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings4.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings5.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings6.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings7.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings8.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings9.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings10.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings11.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings12.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings13.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings14.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings15.rotation += rotation_speed4 * delta
+	$gas_1_sprite/gas_1_rings16.rotation += rotation_speed4 * delta
+	
+	
+	
+	
+	
+	
+	
+	
+	
